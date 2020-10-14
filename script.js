84cc31f46308158be5c4b49fa5b4328c2a1dbcf6
johnnyquinn <jftq97@gmail.com>
2020-10-14 01:34:23 -0400

    added title and placeholder for searchbar. Cleaned up js file

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ const idParty = document.querySelector('#id-party')
 Handler function to evaluate user's input into the search bar
 */
 function evalSearch(){
-    const searchbarValue = searchbarInput.value
-    searchbarValue.toLowerCase()
+    const searchbarValue = (searchbarInput.value).toLowerCase()
     console.log(searchbarValue)
 
     let dataFirstName = ""
@@ -19,12 +18,11 @@ function evalSearch(){
 
     //runs through lawmaker-data.js
     for(let i=0; i<data.length; ++i){
-        console.log(data[i].first_name)
         dataFirstName = (data[i].first_name).toLowerCase()      //converts names from lawmaker-data to lowercase
         dataLastName = (data[i].last_name).toLowerCase()
 
         if(searchbarValue.includes(dataFirstName) || searchbarValue.includes(dataLastName)){ //if search bar input matches then display info
-            idName.innerText = data[i].first_name + data[i].last_name
+            idName.innerText = `${data[i].first_name}  ${data[i].last_name}`
             idState.innerText = data[i].state
             idParty.innerText = data[i].party
         }
